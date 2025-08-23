3c3d938211660a74873004de84486928a60a2698
Dryghost <mandalcyan529@gmail.com>
2025-08-24 02:47:26 +0530

    new

--- index.js
+++ index.js
@@ -1,9 +1,100 @@
-const express = require('express');
-const app = express();
+const express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose");
+// const path = require("path");
+const User = require("./model/user");
+let app = express();
+
+// this is the connecting line for my mongodb database
+mongoose.connect("mongodb://localhost/27017");
 
 
-app.get("/",function(req,res){
-    res.send("Hello");
-})
 
-app.listen(3000)
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Showing home page
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+// Showing secret page
+app.get("/secret", isLoggedIn, function (req, res) {
+    res.render("secret");
+});
+
+// Showing register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+// Handling user signup
+app.post("/register", async (req, res) => {
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    return res.status(200).json(user);
+});
+
+// Showing login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+// Handling user login
+app.post("/login", async function (req, res) {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user) {
+            const result = req.body.password === user.password;
+            if (result) {
+                res.render("secret");
+            } else {
+                res.status(400).json({ error: "password doesn't match" });
+            }
+        } else {
+            res.status(400).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+
+
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+});
